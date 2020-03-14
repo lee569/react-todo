00ed6454e6b830f9b1b3305b32e90677d74dd8d1
lee569 <61532484+lee569@users.noreply.github.com>
2020-03-14 10:09:39 -0400

    added todo feature

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { TodoList } from './TodoList';
+
+
+
+const initialTodos: Array<Todo> = [
+  {text: "Walk the dog", complete: true},
+  {text: "Write app", complete: false}
+];
+const App: React.FC = () =>{
+  const [todos, setTodos] = useState(initialTodos);
+
+const toggleTodo: ToggleTodo = selectedTodo => {
+  const newTodos = todos.map(todo => {
+    if(todo === selectedTodo){
+     return {
+       ...todo,
+       complete: !todo.complete
+     } 
+    }
+    return todo;
+  });
+  setTodos(newTodos);
+};
+
+  return (
+  <React.Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+  </React.Fragment>
+  );
+};
+
+export default App;
